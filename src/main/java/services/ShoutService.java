b3e17efd2f806f6ddb6b6c140068c6efd1215450
Servesus <jesusgarrocho1998@gmail.com>
2018-10-14 19:09:35 +0200

    Caso de estudio 3

---
+++ src/main/java/services/ShoutService.java
@@ -0,0 +1,26 @@
+
+package services;
+
+import java.util.Collection;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import repositories.ShoutRepository;
+import domain.Shout;
+
+@Service
+@Transactional
+public class ShoutService {
+
+	@Autowired
+	private ShoutRepository	shoutRepository;
+
+
+	public Collection<Shout> findAll() {
+		Collection<Shout> result;
+		result = this.shoutRepository.findAll();
+		return result;
+	}
+}
